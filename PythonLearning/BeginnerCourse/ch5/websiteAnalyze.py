38bedba89e696d6b5eb38b9b5099f3c41dbf23b2
Qineria <haochinnate@gmail.com>
2017-03-14 22:56:19 +0800

    urllib package and start learn requests package

--- PythonLearning/BeginnerCourse/ch5/websiteAnalyze.py
+++ PythonLearning/BeginnerCourse/ch5/websiteAnalyze.py
@@ -5,8 +5,29 @@ Created on Mon Mar 13 22:52:51 2017
 @author: haochinnate
 """
 
-# urllib package, urlparse method
+# urllib package, urlparse method will return a tuple ParseResult object
+from urllib.parse import urlparse
 
-# request method 
+url = 'http://taqm.epa.gov.tw:80/pm25/tw/PM25A.aspx?area=1'
+o = urlparse(url)
+
+print(o)
+print("sheme={}-{}".format(o.scheme, o[0]))
+print("netloc={}-{}".format(o.netloc, o[1]))
+print("path={}-{}".format(o.path, o[2]))
+print("params={}-{}".format(o.params,o[3]))
+print("query={}-{}".format(o.query, o[4]))
+print("fragment={}-{}".format(o.fragment, o[5]))
+print("port={}".format(o.port))
+
+
+# requests package, get method
+import requests
+
+url2 = 'http://www.e-happy.com.tw'
+html = requests.get(url2)
+html.encoding = "UTF8"
+print(html.text)  
+
 
 # Beautifulsoup package?
