3b909d194f3b39e5225a7f602db94d4c2beaddd2
Qineria <haochinnate@gmail.com>
2017-03-23 23:24:30 +0800

    setup crawler

--- PythonLearning/BeginnerCourse/ch5/webCrawlerExample.py
+++ PythonLearning/BeginnerCourse/ch5/webCrawlerExample.py
@@ -5,7 +5,17 @@ Created on Wed Mar 22 22:33:07 2017
 @author: haochinnate
 """
 
+import requests
+from bs4 import BeautifulSoup
+
+url = "http://www.cpbl.com.tw/standing/year.html"
+html = requests.get(url)
+sp = BeautifulSoup(html.text, "html.parser")
+
 # class: cont_single
+mainContent = sp.find("div", {"class":"cont_single"})
+print(mainContent)
+
 
 # class: gap_b20
 
