934f1ef9a7b20fab5a55fa090fc3c2bdeca3beda
Qineria <haochinnate@gmail.com>
2017-03-26 23:08:11 +0800

    get the content of the grid

--- PythonLearning/BeginnerCourse/ch5/webCrawlerExample.py
+++ PythonLearning/BeginnerCourse/ch5/webCrawlerExample.py
@@ -14,8 +14,27 @@ sp = BeautifulSoup(html.text, "html.parser")
 
 # class: cont_single
 mainContent = sp.find("div", {"class":"cont_single"})
-print(mainContent)
+#print(mainContent)
 
 
 # class: gap_b20
+allTableTitles = mainContent.find_all("h3", {"class":"gap_t20"})
+#print(allTableTitles)
 
+allTableContents =  mainContent.find_all("table", {"class":"std_tb mix_x"})
+#print(allTableContents)
+
+for tableContent in allTableContents:
+    rows = tableContent.find_all("tr")
+    
+    # grid header
+    for col in rows[0].find_all("th"):
+        print(col.text, end='           ')
+    print()
+    
+    # grid content
+    for i in range(1, len(rows)):
+        for col in rows[i].find_all("td"):
+            print(col.text, end='   ')
+        print()
+         
