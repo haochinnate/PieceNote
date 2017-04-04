5ddb49d9578f7d6720ed7a8e501ba5019890bdb7
Qineria <haochinnate@gmail.com>
2017-04-04 22:17:40 +0800

    use hashlib to get md5 of binary string

--- PythonLearning/BeginnerCourse/ch6/hashlibExample.py
+++ PythonLearning/BeginnerCourse/ch6/hashlibExample.py
@@ -5,3 +5,19 @@ Created on Thu Mar 30 23:02:28 2017
 @author: haochinnate
 """
 
+# use hashlib package to judge the document is updated  or not
+import sqlite3, ast, hashlib, os, requests
+from bs4 import BeautifulSoup
+
+# create a md5 object
+md5 = hashlib.md5()
+
+# encrypt the string (b for binary?)
+md5.update(b'Test String')
+
+# 16bits encrypt result 
+print(md5.hexdigest()) 
+
+
+#---------------------------------
+#conn = sqlite3.connect()
