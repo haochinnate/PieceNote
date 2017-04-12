2fbbe13a9ede62a657f3e5bd423444e8fcc0cca4
Qineria <haochinnate@gmail.com>
2017-04-12 23:24:38 +0800

    start try selenium

--- PythonLearning/BeginnerCourse/ch6/seleniumExample.py
+++ PythonLearning/BeginnerCourse/ch6/seleniumExample.py
@@ -5,3 +5,26 @@ Created on Sun Apr  9 18:08:35 2017
 @author: haochinnate
 """
 
+from selenium import webdriver
+from time import sleep
+
+urlList = ['https://www.u-car.com.tw/', 
+           'http://www.cpbl.com.tw/standing/year.html',
+           'https://www.google.com.tw/'] # ,
+           #'http://www.baseball-reference.com/']
+
+# use selenium to control the browser
+browser = webdriver.Chrome()
+
+browser.maximize_window()
+
+#link to specific website
+for url in urlList:
+    browser.get(url) 
+    sleep(3)
+    
+
+browser.close()
+
+
+
