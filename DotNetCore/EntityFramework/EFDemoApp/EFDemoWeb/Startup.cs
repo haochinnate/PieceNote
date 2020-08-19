844144952ee1fd231b38aab0149504f602f17ad0
Qineria <haochinnate@gmail.com>
2020-08-19 13:03:21 +0800

    create DB context and using it in Startup, next: assign connection string

--- DotNetCore/EntityFramework/EFDemoApp/EFDemoWeb/Startup.cs
+++ DotNetCore/EntityFramework/EFDemoApp/EFDemoWeb/Startup.cs
@@ -2,9 +2,11 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using EFDataAccessLibrary.DataAccess;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -23,6 +25,12 @@ namespace EFDemoWeb
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            // adding our DB context to our dependency injection system
+            services.AddDbContext<PeopleContext>(options =>
+            {
+                // using SQL server, and connection string is from appsettings.json
+                options.UseSqlServer(Configuration.GetConnectionString("Default"));
+            });
             services.AddRazorPages();
         }
 
