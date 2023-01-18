bb6ec0c53fbdb97bde5e0cf88a8515aa2e22d9dd
Qineria <haochinnate@gmail.com>
2023-01-18 11:04:42 +0800

    895, 2515, 926

---
+++ Software/LeetCode/ClosetTarget_2515.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Leetcode
+{
+    internal class ClosetTarget_2515
+    {
+        public int ClosetTarget(string[] words, string target, int startIndex)
+        {
+            List<int> matchTargetDistance = new List<int>();
+            
+            
+            for (int i = 0; i < words.Length; i++)
+            {
+                if (words[i].Equals(target))
+                {
+                    if (startIndex >= i)
+                    {
+                        matchTargetDistance.Add(startIndex - i);
+                        matchTargetDistance.Add(words.Length - startIndex + i);
+
+                    }
+                    else
+                    {
+                        matchTargetDistance.Add(i - startIndex);
+                        matchTargetDistance.Add(startIndex + words.Length - i);
+                    }
+                }
+            }
+
+            if (matchTargetDistance.Count == 0)
+            {
+                return -1;
+            }
+            else
+            {
+                return matchTargetDistance.Min(); 
+            }
+        }
+    }
+}
---
+++ Software/LeetCode/FrequencyStack_895.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Leetcode
+{
+    internal class FrequencyStack_895
+    {
+        public class FreqStack
+        {
+            Dictionary<int, int> numberToTimesMapping = new Dictionary<int, int>();
+            Dictionary<int, Stack<int>> timesToNumbers = new Dictionary<int, Stack<int>>();
+            int maximumFrequency = 0;
+            public FreqStack()
+            {
+
+            }
+
+            public void Push(int val)
+            {
+                if (numberToTimesMapping.ContainsKey(val))
+                {
+                    numberToTimesMapping[val]++;
+                }
+                else
+                {
+                    numberToTimesMapping.Add(val, 1);
+                }
+                
+                maximumFrequency = Math.Max(maximumFrequency, numberToTimesMapping[val]);
+
+                if (timesToNumbers.ContainsKey(numberToTimesMapping[val]))
+                {
+                    timesToNumbers[numberToTimesMapping[val]].Push(val);
+                }
+                else
+                {
+                    var newStack = new Stack<int>();
+                    newStack.Push(val);
+                    timesToNumbers.Add(numberToTimesMapping[val], newStack);
+                }
+            }
+
+            public int Pop()
+            {
+                int highestFrequenct_number = timesToNumbers[maximumFrequency].Pop();
+
+                numberToTimesMapping[highestFrequenct_number]--;
+
+                if (timesToNumbers[maximumFrequency].Count == 0)
+                {
+                    maximumFrequency--;
+                }
+
+                return highestFrequenct_number;                
+            }
+        }
+
+    }
+}
