7f1d0fc68dc1b5c5e1c5d720629fbd021ac74e2f
Qineria <haochinnate@gmail.com>
2023-01-19 11:29:49 +0800

    918 and 974

---
+++ Software/LeetCode/SubarraysDivByK_974.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Leetcode
+{
+    internal class SubarraysDivByK_974
+    {
+        public int SubarraysDivByK(int[] nums, int k)
+        {
+            int result = 0;
+
+            // remainderToFrequencyMap
+            List<int> remainderToFrequencyMap = new List<int>(new int[k])
+            {
+                [0] = 1
+            };
+
+            int sumToCurrentIndex = 0;
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                sumToCurrentIndex += nums[i];
+                
+                int remainder = sumToCurrentIndex % k;
+                if (remainder < 0)
+                {
+                    remainder += k;
+                }
+
+                result += remainderToFrequencyMap[remainder];
+
+                remainderToFrequencyMap[remainder]++;
+            }
+
+            return result;
+        }
+    }
+}
