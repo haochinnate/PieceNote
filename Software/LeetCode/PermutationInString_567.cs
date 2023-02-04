f0f1a27c65f9a1490bda11895115bf862631bfc2
Qineria <haochinnate@gmail.com>
2023-02-04 10:26:31 +0800

    Leetcode 567, 6, 2410

---
+++ Software/LeetCode/PermutationInString_567.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Leetcode
+{
+    internal class PermutationInString_567
+    {
+        public bool CheckInclusion(string s1, string s2)
+        {
+            if (s1.Length > s2.Length)
+            {
+                return false;
+            }
+
+
+            List<int> alphabetFrequencyS1 = new List<int>(26);
+            List<int> alphabetFrequencyS2 = new List<int>(26);
+
+            for (int i = 0; i < 26; i++)
+            {
+                alphabetFrequencyS1.Add(0);
+                alphabetFrequencyS2.Add(0);
+            }
+
+            for (int i = 0; i < s1.Length; i++)
+            {
+                alphabetFrequencyS1[s1[i] - 'a']++;
+                alphabetFrequencyS2[s2[i] - 'a']++;
+            }
+            
+
+            if (IsPermutation(alphabetFrequencyS1, alphabetFrequencyS2))
+            {
+                return true;
+            }
+
+            int leftIndex = 0;
+            for (int rightIndex = s1.Length; rightIndex < s2.Length; rightIndex++)
+            {
+                alphabetFrequencyS2[s2[leftIndex] - 'a']--;
+                leftIndex++;
+                alphabetFrequencyS2[s2[rightIndex] - 'a']++;
+                if (IsPermutation(alphabetFrequencyS1, alphabetFrequencyS2))
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+
+        private bool IsPermutation(List<int> list1, List<int> list2)
+        {
+            for (int i = 0; i < list1.Count; i++)
+            {
+                if (list1[i] != list2[i])
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+    }
+}
---
+++ Software/LeetCode/ZigzagConvert_6.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Leetcode
+{
+    internal class ZigzagConvert_6
+    {
+        public string Convert(string s, int numRows)
+        {
+            if (numRows == 1)
+            {
+                return s;
+            }
+
+            List<StringBuilder> eachRowsContents = new List<StringBuilder>(numRows);
+            for (int i = 0; i < numRows; i++)
+            {
+                eachRowsContents.Add(new StringBuilder());
+            }
+
+
+            int currentRowIndex = 0;
+            bool zigzagDirectionIsDown = true;
+            foreach (var singleChar in s)
+            {
+                eachRowsContents[currentRowIndex].Append(singleChar);
+
+                if (zigzagDirectionIsDown)
+                {
+                    if (currentRowIndex == numRows - 1)
+                    {
+                        zigzagDirectionIsDown = false;
+                        currentRowIndex--;
+                    }
+                    else
+                    {
+                        currentRowIndex++;
+                    }
+                }
+                else
+                {
+                    if (currentRowIndex == 0)
+                    {
+                        zigzagDirectionIsDown = true;
+                        currentRowIndex++;
+                    }
+                    else
+                    {
+                        currentRowIndex--;
+                    }
+                }
+            }
+
+            StringBuilder result = new StringBuilder();
+            foreach (var rowContent in eachRowsContents)
+            {
+                result.Append(rowContent.ToString());
+            }
+
+            return result.ToString();
+        }
+    }
+}
